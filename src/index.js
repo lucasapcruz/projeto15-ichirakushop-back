89f8e3e3567551128ed30e5c561df77b6c842809
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-11-23 13:35:53 -0300

    chore: adjust code for running deploy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running in port: ${port}`));
