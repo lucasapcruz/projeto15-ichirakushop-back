bf2e822fb0daef8a0cc874b63ea954a3baf6b161
Igor-S-M <igorsousa3148@gmail.com>
2022-11-24 13:46:56 -0300

    feat: signIn behavior

---
+++ src/Middlewares/signInValidation.middleware.js
@@ -0,0 +1,22 @@
+import { registeredUsers } from "../database/db"
+import bcrypt from "bcrypt"
+
+export async function signInValidation(req,res,next){
+
+    const user = req.body
+
+    const userExist = await registeredUsers.findOne({email:user.email})
+    if(!userExist){
+        return res.status(401).send("email não cadastrado")
+    }
+
+    const passwordOK = bcrypt.compareSync(user.password, userExist.password)
+
+    if(!passwordOK){
+        return res.status(401).send("senha incorreta")
+    }
+
+    res.locals.user = userExist
+
+    next()
+}
