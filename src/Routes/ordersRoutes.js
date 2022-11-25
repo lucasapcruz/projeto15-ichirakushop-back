bc0cfa3570917c74ab294f33d46724511c24d4c2
Lucas Cruz <lucas.apcruz@gmail.com>
2022-11-25 20:19:17 -0300

    refac: new routed for updating product array on order

--- src/Routes/ordersRoutes.js
+++ src/Routes/ordersRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { deleteOrder } from "../Controllers/orders.controller.js";
-import { createOrder, updateOrder, getOrders } from "../Controllers/orders.controller.js";
+import { createOrder, updateOrderProducts, updateOrderStatus, getOrders } from "../Controllers/orders.controller.js";
 import { authRoutesValidation } from "../Middlewares/authRoutesValidation.middleware.js";
 import { orderCreationValidation } from "../Middlewares/orderCreationSchemaValidation.middleware.js";
 
 const ordersRoute = Router();
 
 ordersRoute.post("/order", authRoutesValidation, orderCreationValidation, createOrder);
-ordersRoute.put("/order/:orderId", authRoutesValidation, updateOrder);
+ordersRoute.put("/order/:orderId", authRoutesValidation, updateOrderStatus);
+ordersRoute.put("/order/:orderId/products", authRoutesValidation, updateOrderProducts);
 ordersRoute.delete("/order/:orderId", authRoutesValidation, deleteOrder);
 ordersRoute.get("/orders", authRoutesValidation, getOrders);
 
