30676fc2eb786e1d67e8fda1c58957a4629ed0e1
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-11-25 17:07:51 -0300

    feat: get bestSellers

--- src/Controllers/products.controller.js
+++ src/Controllers/products.controller.js
@@ -1,8 +1,28 @@
 import { productsCollection } from "../database/db.js"
 
 export async function getProducts(req,res){
+    
+    const isBestSeller = req.query.tag
+
+    function compareAvailable(a,b){
+        if (a.availableQt>b.availableQt){
+            return 1
+        }
+        if (a.availableQt<b.availableQt){
+            return -1
+        }
+        return 0
+    }
     try{
+
         const products = await productsCollection.find({ availableQt : { $gt : 0 } }).toArray()
+
+        if(isBestSeller === "bestSeller"){
+            const bestSellers = products.sort(compareAvailable).slice(0,2)
+            return res.send(bestSellers)
+            
+        }
+        
         res.send(products)
     }catch(err){
         console.log(err)
