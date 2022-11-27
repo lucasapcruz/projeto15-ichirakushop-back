3e012e87169c810fb3f8633f2171cd581116a00f
Lucas Cruz <lucas.apcruz@gmail.com>
2022-11-27 03:45:43 -0300

    refac: order object

--- src/Controllers/orders.controller.js
+++ src/Controllers/orders.controller.js
@@ -1,19 +1,25 @@
-import { ObjectId } from "mongodb";
-import { orders } from "../database/db.js";
+import { Db, ObjectId } from "mongodb";
+import { carts, orders } from "../database/db.js";
 
 export async function createOrder(req, res) {
     const user = res.locals.user;
     const userId = user._id.toString()
     const { cartId } = req.body
 
-    const orderRecord = {
-        userId,
-        cartId,
-        createdDate: Date.now(),
-        updatedDate: Date.now()
-    }
-
     try {
+        const cart = await carts.findOne(new ObjectId(cartId))
+        const shippingAdress = `${user.adress}, ${user.houseNumber}`
+
+        const orderRecord = {
+            userId,
+            shippingAdress,
+            products: cart.products,
+            totalAmout: cart.totalPrice, 
+            createdDate: Date.now(),
+            updatedDate: Date.now()
+        }
+
+
         await orders.insertOne(orderRecord);
         res.status(201).send(orderRecord);
     } catch (err) {
